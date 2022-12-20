64480600debddf187e261d324553a0561fda1e75
Waqas Hayder <waqashayder01@gmail.com>
2022-12-20 16:47:42 +0500

    danish

--- resources/views/frontend/user_profile.blade.php
+++ resources/views/frontend/user_profile.blade.php
@@ -40,37 +40,21 @@
                 <div id="London" class="tabcontent">
                     <h3>London</h3>
                     <p>London is the capital city of England.</p>
+
                 </div>
 
                 <div id="Paris" class="tabcontent">
                     <h3>Paris</h3>
                     <p>Paris is the capital of France.</p>
-                </div>
-
-                <div id="Tokyo" class="tabcontent">
-                    <h3>Tokyo</h3>
-                    <p>Tokyo is the capital of Japan.</p>
+                    <p>Paris i e capita of France.< <p>Tokyo is the capital f Japan.</p>
+                    </di </di>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <script>
-        function openCity(evt, cityName) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-                tabcontent[i].style.display = "none";
-            }
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-            document.getElementById(cityName).style.display = "block";
-            evt.currentTarget.className += " active";
-        }
-
-        // Get the element with id="defaultOpen" and click on it
-        document.getElementById("defaultOpen").click();
-    </script>
-@endsection
+            <scri func tion op enCity(e cityNam var i, tabcontent, tablinks;
+                tabcontent=document.getElementsByClassName("tabcontent"); for (i=0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display="none" ; } tablinks=document.getElementsByClassName("tablinks"); for (i=0; i <
+                tablinks.length; i++) { tablinks[i].className=tablinks[i].className.replace(" active", "" ); }
+                document.getElementById(cityName).style.display="block" ; evt.currentTarget.className +=" active" ; } // Get
+                the element with id="defaultOpen" and click on it document.getElementById("defaultOpen").click(); </script>
+            @endsection
