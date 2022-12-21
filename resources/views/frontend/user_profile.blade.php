634d7edaf8b03d1332207d720c32e4a65493c63e
Waqas Hayder <waqashayder01@gmail.com>
2022-12-21 13:12:30 +0500

    1

--- resources/views/frontend/user_profile.blade.php
+++ resources/views/frontend/user_profile.blade.php
@@ -20,28 +20,28 @@
                         <a class="tablinks text_d_none d-flex align-items-center border_none mb-3"
                             onclick="openCity(event, 'Paris')">
                             <div class="user_icon_span">
-                                <i classfa-volume-low-motorcycle user_icon"></i>
+                                <i class="fa-solid fa-volume-low user_icon"></i>
                             </div>
                             <span class="user_pro_cat ms-2">My Adverts</span>
                         </a>
                         <a class="tablinks text_d_none d-flex align-items-center border_none mb-3"
                             onclick="openCity(event, 'Tokyo')">
                             <div class="user_icon_span">
-                                a-regular fa-bell-motorcycle user_icon"></i>
+                                <i class="fa-regular fa-bell user_icon"></i>
                             </div>
                             <span class="user_pro_cat ms-2">Notification</span>
                         </a>
                         <a class="tablinks text_d_none d-flex align-items-center border_none mb-3"
                             onclick="openCity(event, 'Save')">
                             <div class="user_icon_span">
-                                <i class fa-bookmark-motorcycle user_icon"></i>
+                                <i class="fa-solid  fa-bookmark user_icon"></i>
                             </div>
                             <span class="user_pro_cat ms-2">My Save</span>
                         </a>
                         <a class="tablinks text_d_none d-flex align-items-center border_none mb-3"
                             onclick="openCity(event, 'Message')">
                             <div class="user_icon_span">
-                                fa-regular fa-envelope-motorcycle user_icon"></i>
+                                <i class="fa-regular fa-envelope user_icon"></i>
                             </div>
                             <span class="user_pro_cat ms-2">Message</span>
                         </a>
@@ -50,64 +50,68 @@
                     <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button> --}}
                 </div>
             </div>
-            <div 9lass="col-8 p-4">
-                <div id="London" class=shadow"t <div class="row">
-                    <div class="col-12">
-                        <div class="cat_content_heading">
-                            <h3 class="">Account Settings</h3>
+            <div class="col-9 p-4 ">
+                <div id="London" class="tabcontent shadow">
+                    <div class="row">
+                        <div class="col-12 mb-5">
+                            <div class="cat_content_heading">
+                                <h3 class="">Account Settings</h3>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-6">
-                        <label class="form-label" id="label-form">Fa Name</label>
-                        <div class="sectionblock space long-value-input-container">
-                            <input type="text" required class="long-value-input" value=""
-                                placeholder="Last Name" />
+                        <div class="col-6">
+                            <label class="form-label" id="label-form">First Name</label>
+                            <div class="sectionblock1 space long-value-input-container">
+                                <input type="text" required class="long-value-input" value=""
+                                    placeholder="First Name" />
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-6">
-                        <label class="form-label" id="label-form">Last Name</label>
-                        <div class="sectionblock space long-value-input-container">
-                            <input type="text" required class="long-value-input" value=""
-                                placeholder="Last Name" />
+                        <div class="col-6">
+                            <label class="form-label" id="label-form">Last Name</label>
+                            <div class="sectionblock1 space long-value-input-container">
+                                <input type="text" required class="long-value-input" value=""
+                                    placeholder="Last Name" />
+                            </div>
                         </div>
                     </div>
                 </div>
-                gland.</p>
 
-            </div>
 
-            <div id="Paris" class="tabcontent">
-                <h3>Paris</h3>
-                <p>Paris is the capital of France.</p>
-            </div>
-            <div id="Tokyo" class="tabcontent">
-                <h3>Tokyo</h3>
-                <p>Tokyo is the capital of iTLY .</p>
-            </div>
-            <div id="Save" class="tabcontent">
-                <h3>save</h3>
 
-            </div>
-            <div id="Message" class="tabcontent">
-                <h3>Message</h3>
 
+                <div id="Paris" class="tabcontent">
+                    <h3>Paris</h3>
+                    <p>Paris is the capital of France.</p>
+                </div>
+                <div id="Tokyo" class="tabcontent">
+                    <h3>Tokyo</h3>
+                    <p>Tokyo is the capital of iTLY .</p>
+                </div>
+                <div id="Save" class="tabcontent">
+                    <h3>save</h3>
+
+                </div>
+                <div id="Message" class="tabcontent">
+                    <h3>Message</h3>
+
+                </div>
             </div>
         </div>
-    </div>
-    <script>
-        function openCity(evt, cityName) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-                tabcontent[i].style.display = "none";
+        <script>
+            function openCity(evt, cityName) {
+                var i, tabcontent, tablinks;
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                document.getElementById(cityName).style.display = "block";
+                evt.currentTarget.className += " active";
             }
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-            document.getElementById(cityName).style.display = "block";
-            evt.currentTarget.className += " active";
-        }
 
-        // Get the element with id="defaultOpen" and click on it
-        document.getElementById("defaultOpen").click();
+            // Get the element with id="defaultOpen" and click on it
+            document.getElementById("defaultOpen").click();
+        </script>
+    @endsection
