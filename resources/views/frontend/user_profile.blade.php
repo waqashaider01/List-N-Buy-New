145d2f43c43ea0bdf9ce5d8dc4c5cb6bbe98bdce
Waqas Hayder <waqashayder01@gmail.com>
2022-12-21 15:55:34 +0500

    2

--- resources/views/frontend/user_profile.blade.php
+++ resources/views/frontend/user_profile.blade.php
@@ -52,26 +52,90 @@
             </div>
             <div class="col-9 p-4 ">
                 <div id="London" class="tabcontent shadow">
-                    <div class="row">
+                    <div class="row p-3">
                         <div class="col-12 mb-5">
                             <div class="cat_content_heading">
                                 <h3 class="">Account Settings</h3>
                             </div>
                         </div>
-                        <div class="col-6">
-                            <label class="form-label" id="label-form">First Name</label>
+                        <div class="col-md-6 mb-4">
+                            <label class="form-label1" id="label-form">First Name</label>
                             <div class="sectionblock1 space long-value-input-container">
                                 <input type="text" required class="long-value-input" value=""
                                     placeholder="First Name" />
                             </div>
                         </div>
-                        <div class="col-6">
-                            <label class="form-label" id="label-form">Last Name</label>
+                        <div class="col-md-6 mb-4">
+                            <label class="form-label1" id="label-form">Last Name</label>
                             <div class="sectionblock1 space long-value-input-container">
                                 <input type="text" required class="long-value-input" value=""
                                     placeholder="Last Name" />
                             </div>
                         </div>
+                        <div class="col-md-6 mb-4">
+                            <label class="form-label1" id="label-form">Email</label>
+                            <div class="sectionblock1 space long-value-input-container">
+                                <input type="email" required class="long-value-input" value=""
+                                    placeholder="Email" />
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <label class="form-label1" id="label-form">Phone Number</label>
+                            <div class="sectionblock1 space long-value-input-container">
+                                <input type="Number" required class="long-value-input" value=""
+                                    placeholder="Phone Number" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label1">Password</label>
+                            <div class="sectionblock1 space long-value-input-container">
+                                <input id="pswrd" type="password" placeholder="Password" class="long-value-input">
+                                <span style="position: absolute; right:-10px;">
+                                    <i class="bi bi-lock showclose" onclick="show()" style="font-size: 20px;"></i>
+                                    <i class="bi bi-unlock showopen" style="display: none; font-size: 20px;"
+                                        onclick="showopen()"></i>
+                                </span>
+                            </div>
+                            <script>
+                                function show() {
+                                    var pswrd = document.getElementById('pswrd');
+                                    var icon = document.querySelector('.showclose');
+                                    var iconopen = document.querySelector('.showopen');
+
+                                    if (pswrd.type === "password") {
+                                        pswrd.type = "text";
+                                        //
+                                        icon.style.display = 'none';
+                                        iconopen.style.display = 'block';
+
+
+                                    } else {
+                                        pswrd.type = "password";
+                                        icon.style.color = "grey";
+                                    }
+                                }
+
+                                function showopen() {
+                                    var pswrd = document.getElementById('pswrd');
+                                    var icon = document.querySelector('.showclose');
+                                    var iconopen = document.querySelector('.showopen');
+
+                                    if (pswrd.type === "text") {
+                                        pswrd.type = "password";
+                                        //
+                                        icon.style.display = 'flex';
+                                        icon.style.marginTop = '15px';
+                                        iconopen.style.display = 'none';
+
+
+                                    } else {
+                                        pswrd.type = "password";
+                                        icon.style.color = "grey";
+                                    }
+                                }
+                            </script>
+                        </div>
+
                     </div>
                 </div>
 
