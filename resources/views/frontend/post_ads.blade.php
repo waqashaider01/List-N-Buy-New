6ee32e656c230a0d795ed3ea10329aa2ae82399c
Waqas Hayder <waqashayder01@gmail.com>
2022-12-28 18:11:31 +0500

    changes editing

--- resources/views/frontend/post_ads.blade.php
+++ resources/views/frontend/post_ads.blade.php
@@ -19,20 +19,20 @@
             <ul class="nav nav-pills mb-3 justify-content-between" id="pills-tab" role="tablist">
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active s_about_ad mb-3" id="pills-home-tab" data-bs-toggle="pill"
-                        data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
+                        data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-home"
                         aria-selected="true">Step1: About
                         Ad</button>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link s_about_ad2 mb-3" id="pills-profile-tab" data-bs-toggle="pill"
-                        data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile"
+                    <button class="nav-link  s_about_ad2 mb-3" id="pills-profile-tab" data-bs-toggle="pill"
+                        data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-profile"
                         aria-selected="false">Step 2: Ad
                         Details</button>
                 </li>
 
             </ul>
             <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"
+                <div class="tab-pane fade  " id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"
                     tabindex="0">
                     <form action="" class="pt-5">
 
@@ -400,8 +400,8 @@
 
                     </form>
                 </div>
-                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"
-                    tabindex="0">
+                <div class="tab-pane fade show active" id="pills-profile" role="tabpanel"
+                    aria-labelledby="pills-profile-tab" tabindex="0">
 
                     <div class="container">
                         <div class="row d-flex justify-content-between g-5">
